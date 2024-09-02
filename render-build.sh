2bc83d3901603734054ffa8993a4b113bc386ba3
smz-exe <shimizu-yuki13861@keio.jp>
2024-09-02 14:24:09 +0900

    chore: Create Puppeteer Cache Directory in render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -7,6 +7,8 @@ npx puppeteer install
 
 # Store/pull Puppeteer cache with build cache
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+  echo "...Creating Puppeteer Cache Directory"
+  mkdir -p $PUPPETEER_CACHE_DIR
   echo "...Copying Puppeteer Cache from Build Cache"
   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
 else
